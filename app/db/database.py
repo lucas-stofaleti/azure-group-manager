7aada3188aef0121bb723e042f819296c7481bb3
Lucas Stofaleti <lucas.stofaleti@asml.com>
2023-05-02 16:25:30 +0200

    feat: change template used

--- app/db/database.py
+++ app/db/database.py
@@ -4,5 +4,11 @@ from app.utils.config import settings
 client = pymongo.MongoClient(f"mongodb+srv://{settings.mongo_user}:{settings.mongo_password}@{settings.mongo_url}/?retryWrites=true&w=majority")
 db = client.group_manager
 
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
 def get_connection():
     return db
