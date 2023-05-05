d4a7802f91c45f5d21c9acd49bcff95b4d900ede
Lucas Stofaleti <lucas.stofaleti@asml.com>
2023-05-05 16:20:27 +0200

    feat: change tip

--- app/routes/groups.py
+++ app/routes/groups.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from app.utils.identity import requires_auth
+from app.db.database import get_connection
+from app.db.crud import *
 
 templates = Jinja2Templates(directory="app/templates")
 
@@ -14,4 +16,13 @@ group_router = APIRouter(
 def groups(request: Request):
     return templates.TemplateResponse(
         "pages/groups.html", {"request": request}
+    )
+
+@group_router.get("/groups")
+@requires_auth
+def groups(request: Request, db = Depends(get_connection), membership: str = "all"):
+    groups = list(get_groups(db))
+    print(groups)
+    return templates.TemplateResponse(
+        "partials/group_table.html", {"request": request, "groups": groups}
     )
